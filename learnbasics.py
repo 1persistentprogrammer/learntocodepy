d5879466c762b3551ff94e15fcf88376344ff5be
1persistentprogrammer <63676265+1persistentprogrammer@users.noreply.github.com>
2020-07-05 11:52:57 -0400

    learnbasics.py

---
+++ learnbasics.py
@@ -0,0 +1,25 @@
+#Example 1 
+item1 = 100
+item2 = 100
+total = item1 + item2
+print(total)
+
+#Example 2
+friends = ['Annie', 'Lucy', 'Kate', 'David']
+for friend in friends:
+    #Example 3
+    print(friend)
+
+#Example 4
+friends = ['Annie', 'Lucy', 'Kate', 'David']
+for friend in friends:
+    if friend.startswith("A"):      
+        print(friend)
+
+#Example 5
+friends = ['Annie', 'Lucy', 'Kate', 'David']
+i=0
+while friends and i < 3:
+    print(friends[i])
+    i+=1
+
